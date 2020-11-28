6a2b01658f7ebf41646df91c41d66703c7eceb44
thomaslnx <marcos.msilva10@gmail.com>
2020-11-28 18:25:44 -0300

    Modify dabase address connect

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -4,10 +4,11 @@ class Database{
   constructor() {
     this.mongo();
   }
+  // 'mongodb://192.168.1.104:27017/tractian',
 
   mongo() {
       this.mongoConnection = mongoose.connect(
-        'mongodb://192.168.1.104:27017/tractian',
+        'mongodb://127.0.0.1:27017/tractian',
         { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
       ).catch(error => console.log('Erro ao conectar ao banco de dados: ',error.message));
   }
