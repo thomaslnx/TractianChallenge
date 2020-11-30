3908b3bf29e93c4fe58eb2caf897ee29d4afad14
thomaslnx <marcos.msilva10@gmail.com>
2020-11-29 23:04:38 -0300

    Modify dabase address connect

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -5,10 +5,11 @@ class Database{
     this.mongo();
   }
   // 'mongodb://192.168.1.104:27017/tractian',
+  // 'mongodb://200.101.111.185:27017/tractian',
 
   mongo() {
       this.mongoConnection = mongoose.connect(
-        'mongodb://127.0.0.1:27017/tractian',
+        'mongodb+srv://thomaslnx:tractian@tractian.hgwr9.mongodb.net/tractian?retryWrites=true&w=majority',
         { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
       ).catch(error => console.log('Erro ao conectar ao banco de dados: ',error.message));
   }
